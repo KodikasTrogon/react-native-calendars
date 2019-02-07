4f0c02cb4eeec9f2cd49cfe02a634898282a6714
KodikasTrogon <renz.serrano.rnr@gmail.com>
2019-02-07 12:37:54 +0800

    Edit styles in calendar

--- src/style.js
+++ src/style.js
@@ -1,41 +1,42 @@
 import {Platform} from 'react-native';
 
-export const foregroundColor = '#ffffff';
-export const backgroundColor = '#f4f4f4';
+export const foregroundColor = '#191919';
+export const backgroundColor = '#191919';
 export const separatorColor = '#e8e9ec';
 
 export const processedColor = '#a7e0a3';
 export const processingColor = '#ffce5c';
-export const failedColor = 'rgba(246, 126, 126,1)';
+export const failedColor = 'rgba(96, 126, 126,1)';
 
-export const textDefaultColor = '#2d4150';
-export const textColor = '#43515c';
-export const textLinkColor = '#00adf5';
-export const textSecondaryColor = '#7a92a5';
+export const textDefaultColor = '#cccccc';
+export const textColor = '#cccccc';
+export const textLinkColor = '#ffffff';
+export const textSecondaryColor = '#666666';
 
 export const textDayFontFamily = 'System';
 export const textMonthFontFamily = 'System';
 export const textDayHeaderFontFamily = 'System';
 
-export const textMonthFontWeight = '300';
+export const textMonthFontWeight = '100';
 
 export const textDayFontSize = 16;
-export const textMonthFontSize = 16;
+export const textMonthFontSize = 18;
 export const textDayHeaderFontSize = 13;
 
 export const calendarBackground = foregroundColor;
-export const textSectionTitleColor = '#b6c1cd';
-export const selectedDayBackgroundColor = textLinkColor;
-export const selectedDayTextColor = foregroundColor;
+export const textSectionTitleColor = '#666666';
+export const selectedDayBackgroundColor = 'rgba(96, 96, 96,0.5)';
+export const selectedDayTextColor = '#fff';
 export const todayBackgroundColor = undefined;
 export const todayTextColor = textLinkColor;
-export const dayTextColor = textDefaultColor;
+export const dayTextColor = '#666666';
 export const textDisabledColor = '#d9e1e8';
 export const dotColor = textLinkColor;
-export const selectedDotColor = foregroundColor;
+export const selectedDotColor = '#fff';
 export const arrowColor = textLinkColor;
-export const monthTextColor = textDefaultColor;
-export const agendaDayTextColor = '#7a92a5';
-export const agendaDayNumColor = '#7a92a5';
+export const monthTextColor = '#ffffff';
+export const agendaDayTextColor = '#666666';
+export const agendaDayNumColor = '#666666';
 export const agendaTodayColor = textLinkColor;
 export const agendaKnobColor = Platform.OS === 'ios' ? '#f2F4f5' : '#4ac4f7';
+
