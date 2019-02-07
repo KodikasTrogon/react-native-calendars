8912b2d847389d2ffbad39a3dc7046f2054ac2e8
KodikasTrogon <renz.serrano.rnr@gmail.com>
2019-02-07 12:53:05 +0800

    Add name credit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//KodikasTrogon Calendar
 export { default as Calendar } from './calendar';
 export { default as CalendarList } from './calendar-list';
 export { default as Agenda } from './agenda';
